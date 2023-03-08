676e28274b6ef8b33787adbaecffcec339af03d8
onionie <gutierrez.onionie@gmail.com>
2023-03-08 00:25:53 -0800

    Created Hero section with body description

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -2,7 +2,24 @@ import React from 'react';
 import './styles.css';
 
 const Hero = () => {
-  return <section className="hero"></section>;
+  return (
+    <section className="hero">
+      <div className="hero__textbox">
+        <div className="hero__textbox__title__container">
+          <div className="hero__textbox__title">
+            Hello! I'm Oniel Gutierrez.
+          </div>
+        </div>
+        <p className="hero__textbox__body">
+          I'm a Software Developer with a passion to learn and create. I am
+          fascinated with building things that live on the internet.
+          <br />
+          Although I love front-end development, I also like to work with
+          back-end technologies such as Node.Js, Express.js and MongoDB.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
