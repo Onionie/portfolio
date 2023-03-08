327c20d09f2a80f5ef4f5aaaaab09a065a5e5ece
onionie <gutierrez.onionie@gmail.com>
2023-03-07 22:59:43 -0800

    Created a Hero component with responsive background image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Header from './Components/Header';
+import Hero from './Components/Hero';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Hero.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const Hero = () => {
+  return <section className="hero"></section>;
+};
+
+export default Hero;
