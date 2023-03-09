0e1efe06bfd87a62b54a5c7d9889bc30997c5909
onionie <gutierrez.onionie@gmail.com>
2023-03-09 02:09:25 -0800

    Created skills component with color gradient for background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './Components/Header';
 import Hero from './Components/Hero';
+import Skills from './Components/Skills';
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Skills />
     </>
   );
 }
---
+++ src/Components/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Skills.module.css';
+
+const Skills = () => {
+  return (
+    <section className={styles.skills}>
+      <div className={styles.skills__box}></div>
+    </section>
+  );
+};
+
+export default Skills;
