8eda8a91ce35e0a029fb20dc21ea10391cf8aa0e
onionie <gutierrez.onionie@gmail.com>
2023-03-09 02:36:16 -0800

    Creates list of skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -4,7 +4,26 @@ import styles from './Skills.module.css';
 const Skills = () => {
   return (
     <section className={styles.skills}>
-      <div className={styles.skills__box}></div>
+      <div className={styles.skills__box}>
+        <h1>Skills</h1>
+        <div className={styles.skills_logo_box}>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+          </ul>
+          <ul>
+            <li>React</li>
+            <li>Bootstrap</li>
+            <li>Git</li>
+          </ul>
+          <ul>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>MongoDB</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
