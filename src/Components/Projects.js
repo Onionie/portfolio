4b43ad15428850b45231ef4c79cd8f02ebe16def
onionie <gutierrez.onionie@gmail.com>
2023-03-10 00:08:08 -0800

    Working state for tabs in the project section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Projects.module.css';
 
 const Projects = () => {
+  const [eFit, setEFit] = useState(true);
+  const [palawan, setPalawan] = useState(false);
+  const [weatherApp, setWeatherApp] = useState(false);
+
+  const eFitHandler = () => {
+    setEFit(true);
+    setPalawan(false);
+    setWeatherApp(false);
+  };
+
+  const palawanHandler = () => {
+    setEFit(false);
+    setPalawan(true);
+    setWeatherApp(false);
+  };
+
+  const weatherHandler = () => {
+    setEFit(false);
+    setPalawan(false);
+    setWeatherApp(true);
+  };
+
   return (
-    <section className={styles.projects}>
+    <section id="projects" className={styles.projects}>
       <h1 className={styles.projects__title}>Projects</h1>
       <ul className={styles.projects__tabs}>
-        <li className={styles.tab__active}>eFitness</li>
-        <li>Palawan</li>
-        <li>Weather App</li>
+        <li onClick={eFitHandler} className={eFit ? styles.tab__active : ''}>
+          eFitness
+        </li>
+        <li
+          onClick={palawanHandler}
+          className={palawan ? styles.tab__active : ''}
+        >
+          Palawan
+        </li>
+        <li
+          onClick={weatherHandler}
+          className={weatherApp ? styles.tab__active : ''}
+        >
+          Weather App
+        </li>
       </ul>
     </section>
   );
