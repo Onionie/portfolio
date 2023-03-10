7426777600c03dbcf75cb5cc1ded103d3ef26281
onionie <gutierrez.onionie@gmail.com>
2023-03-09 21:59:27 -0800

    Created a pill-styled button tabs for project section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,6 +5,11 @@ const Projects = () => {
   return (
     <section className={styles.projects}>
       <h1 className={styles.projects__title}>Projects</h1>
+      <ul className={styles.projects__tabs}>
+        <li className={styles.tab__active}>eFitness</li>
+        <li>Palawan</li>
+        <li>Weather App</li>
+      </ul>
     </section>
   );
 };
