48fb5ffb27920f34dcb5aa10f20c59f90f97f0fe
onionie <gutierrez.onionie@gmail.com>
2023-03-11 15:32:55 -0800

    Created a structure for Contract component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './Components/Header';
 import Hero from './Components/Hero';
 import Skills from './Components/Skills';
 import Projects from './Components/Projects';
+import Contact from './Components/Contact';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Skills />
       <Projects />
+      <Contact />
     </>
   );
 }
---
+++ src/Components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Contact.module.css';
+
+const Contact = () => {
+  return (
+    <section id="contact" className={styles.contact}>
+      <div className={styles.form__container}>
+        <div className={styles.contact__box}></div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
