0f7361248bae59e4ae4916b2bba0d6922bcfcab1
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-04-04 00:14:11 -0700

    Update Hero Section responsiveness

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import './Hero.css';
+import styles from './Hero.module.css';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
@@ -8,15 +8,15 @@ const Hero = () => {
     Aos.init({ duration: 1500, once: true }, []);
   });
   return (
-    <section className="hero__section">
-      <div className="hero__textbox">
-        <div className="hero__textbox__title__container">
-          <div className="hero__textbox__title">
+    <section className={styles.hero__section}>
+      <div className={styles.hero__textbox}>
+        <div className={styles.hero__textbox__title__container}>
+          <div className={styles.hero__textbox__title}>
             Hello! I'm Oniel Gutierrez.
           </div>
         </div>
         <p
-          className="hero__textbox__body"
+          className={styles.hero__textbox__body}
           data-aos="fade-right"
           data-aos-delay="1900"
         >
@@ -29,7 +29,7 @@ const Hero = () => {
         </p>
         <div data-aos="fade-right" data-aos-delay="2100">
           <a
-            className="btn btn__full"
+            className={`${styles.btn} ${styles.btn__full}`}
             href="https://drive.google.com/file/d/1LxytGstwINjYYcxQ3yLlVtOtggomK0GC/"
             target="_blank"
             rel="noreferrer"
@@ -37,7 +37,7 @@ const Hero = () => {
             My Resume
           </a>
           <a
-            className="btn btn__outline"
+            className={`${styles.btn} ${styles.btn__outline}`}
             href="https://github.com/Onionie?tab=repositories"
           >
             My Repositories
