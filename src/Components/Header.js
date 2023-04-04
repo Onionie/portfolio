c3e96e5baff41232383579f4f09d1ab15c85cf73
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-04-03 22:53:12 -0700

    Started working on outline for hamburger menu on mobile

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { BsGithub } from 'react-icons/bs';
-import { BsLinkedin } from 'react-icons/bs';
+import React, { useState } from 'react';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { IoMenu, IoClose } from 'react-icons/io5';
 import styles from './Header.module.css';
 
 const Nav = () => {
+  const [hamburger, setHamburger] = useState(false);
+
+  const hamburgerHandler = () => {
+    setHamburger(!hamburger);
+  };
+
   return (
-    <header>
+    <header className={hamburger ? styles.mobile__nav : ''}>
       <nav>
         <ul>
           <li>
@@ -30,6 +36,14 @@ const Nav = () => {
             </a>
           </li>
         </ul>
+        <div className={styles.btn__mobile}>
+          <IoMenu onClick={hamburgerHandler} />
+          <IoClose
+            className={
+              hamburger ? styles.close__btn__hide : styles.close__btn__show
+            }
+          />
+        </div>
       </nav>
     </header>
   );
